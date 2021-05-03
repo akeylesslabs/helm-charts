69fdd864a54b1fafe42fd195c291285ca6224ca7
Niv Ezrati <niv.e@akeyless.io>
2021-05-03 11:56:55 +0300

    asm-1514 corrected test

--- .github/scripts/apigw_latest_version_release.sh
+++ .github/scripts/apigw_latest_version_release.sh
@@ -9,21 +9,17 @@ service=$(echo "$GITHUB_CONTEXT" | jq -r '.payload.service | select (.!=null)')
 major_minor_patch=$(echo "$GITHUB_CONTEXT" | jq -r '.payload.major_minor_patch | select (.!=null)')
 app_version=$(echo "$GITHUB_CONTEXT" | jq -r '.payload.app_version | select (.!=null)')
 
-# GITHUB_WORKSPACE=$dir_path/../../charts/akeyless-api-gateway # testing
-# major_minor_patch="minor" # testing
-# app_version="1.2.5" # testing
-
-pushd "$GITHUB_WORKSPACE"/charts/akeyless-api-gateway/ # edit 
+pushd "$GITHUB_WORKSPACE"/charts/akeyless-api-gateway/
 # bump chart version
     chart_version=$(grep '^version:[[:space:]][[:digit:]]' Chart.yaml | awk '{print $2}') || die "Failed to retrieve current chart version"
     bump_version "${chart_version}" "${major_minor_patch}"
     new_chart_version=$new_version && echo "The new Chart version is: ${new_chart_version}"
-    sed -i "s/version:.*/version: ${new_chart_version}/g" Chart.yaml # check without "" for ubuntu
+    sed -i "s/version:.*/version: ${new_chart_version}/g" Chart.yaml
 
 # edit app version
-    sed -i "s/appVersion.*/appVersion: ${app_version}/g" Chart.yaml # check without "" for ubuntu
+    sed -i "s/appVersion.*/appVersion: ${app_version}/g" Chart.yaml
 
-    git add -A && git commit -m "Updated helm chart to latest api-gw version" || die "Failed to commit changes to git"
+    git add -A && git commit -m "Updated helm chart to latest api-gw version ${app_version}" || die "Failed to commit changes to git"
     git push origin HEAD
 popd
 
