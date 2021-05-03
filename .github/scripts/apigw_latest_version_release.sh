29f05d28f61667d97f39307a224496de9118cb7e
Niv Ezrati <niv.e@akeyless.io>
2021-05-03 11:48:36 +0300

    asm-1514 api-gw version upgrade test

--- .github/scripts/apigw_latest_version_release.sh
+++ .github/scripts/apigw_latest_version_release.sh
@@ -1,63 +1,33 @@
 #!/usr/bin/env bash
 
-set -x 
+set -e
+
+dir_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "${dir_path}/common.sh"
 
 service=$(echo "$GITHUB_CONTEXT" | jq -r '.payload.service | select (.!=null)')
-publish_as_latest=$(echo "$GITHUB_CONTEXT" | jq -r '.payload.publish_as_latest | select (.!=null)')
 major_minor_patch=$(echo "$GITHUB_CONTEXT" | jq -r '.payload.major_minor_patch | select (.!=null)')
+app_version=$(echo "$GITHUB_CONTEXT" | jq -r '.payload.app_version | select (.!=null)')
 
-echo "test"
-echo $service
-echo $major_minor_patch
-echo $publish_as_latest
+# GITHUB_WORKSPACE=$dir_path/../../charts/akeyless-api-gateway # testing
+# major_minor_patch="minor" # testing
+# app_version="1.2.5" # testing
 
+pushd "$GITHUB_WORKSPACE"/charts/akeyless-api-gateway/ # edit 
+# bump chart version
+    chart_version=$(grep '^version:[[:space:]][[:digit:]]' Chart.yaml | awk '{print $2}') || die "Failed to retrieve current chart version"
+    bump_version "${chart_version}" "${major_minor_patch}"
+    new_chart_version=$new_version && echo "The new Chart version is: ${new_chart_version}"
+    sed -i "s/version:.*/version: ${new_chart_version}/g" Chart.yaml # check without "" for ubuntu
 
+# edit app version
+    sed -i "s/appVersion.*/appVersion: ${app_version}/g" Chart.yaml # check without "" for ubuntu
 
+    git add -A && git commit -m "Updated helm chart to latest api-gw version" || die "Failed to commit changes to git"
+    git push origin HEAD
+popd
 
-# set -e
-
-# function die() {
-#   echo $*
-#   exit 1
-# }
-
-# dir_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-# source "${dir_path}/common.sh"
-
-# lookup_changed_charts() {
-#   commit=$(lookup_latest_tag)
-#   changed_files=$(git diff --find-renames --name-only "$commit" -- charts)
-
-#   local depth=$(( $(tr "/" "\n" <<< charts | wc -l) + 1 ))
-#   local fields="1-${depth}"
-
-#   cut -d '/' -f "$fields" <<< "$changed_files" | uniq
-# }
-
-# main() {
-#   local repo_root
-#   repo_root=$(git rev-parse --show-toplevel)
-#   pushd "$repo_root" > /dev/null
-
-#   local changed_charts=($(lookup_changed_charts | awk '{print $1}'))
-
-#   if [[ -n "${changed_charts[*]}" ]]; then
-#       for chart in "${changed_charts[@]}"; do
-#           if [[ -d "$chart" ]]; then
-#             chart_name=$(helm show chart "$chart" | grep name: | awk '{print $2}')
-#             chart_version=$(helm show chart "$chart" | grep version: | awk '{print $2}')
-#             git tag "$chart_name-$chart_version" || die "Chart version already exists, please bump the version"
-#           else
-#             echo "Chart '$chart' no longer exists in repo. Skipping it..."
-#           fi
-#       done
-#   else
-#       echo "Nothing to do. No chart changes detected."
-#   fi
-
-#   popd > /dev/null
-# }
-
-# main "$@"
+echo "Api-gw app version was successfully updated to latest: ${app_version}"
+echo "Api-gw Helm chart version was updated to: ${new_chart_version}"
 
 
--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -9,3 +9,45 @@ lookup_latest_tag() {
         git rev-list --max-parents=0 --first-parent HEAD
     fi
 }
+
+function die() {
+  echo $*
+  exit 1
+}
+
+function bump_version() {
+  current_version=$1
+  bump_target=$2
+
+  major_version=$(echo "${current_version}" | cut -d'.' -f 1)
+  minor_version=$(echo "${current_version}" | cut -d'.' -f 2)
+  patch_version=$(echo "${current_version}" | cut -d'.' -f 3)
+
+  # Validate each version is an actual number
+  re='^[0-9]+$'
+  if ! [[ $major_version =~ $re ]] ; then
+   die "error: major version target $major_version is not a number!" >&2
+  fi
+  if ! [[ $minor_version =~ $re ]] ; then
+   die "error: minor version target $minor_version is not a number!" >&2
+  fi
+  if ! [[ $patch_version =~ $re ]] ; then
+   die "error: patch version target $patch_version is not a number!" >&2
+  fi
+
+  if [[ "${bump_target}" == "major" ]]; then
+    major_version=$((major_version+1))
+    minor_version=0
+    patch_version=0
+  elif [[ "${bump_target}" == "minor" ]]; then
+    minor_version=$((minor_version+1))
+    patch_version=0
+  elif [[ "${bump_target}" == "patch" ]]; then
+    patch_version=$((patch_version+1))
+
+  else
+    die "Bad parameter - ${bump_target}"
+  fi
+
+  new_version="${major_version}.${minor_version}.${patch_version}"
+}
