662b653d6bbb5a1caddc07ed6e7c0022be48f201
omriezra <omri.e@akeyless.io>
2022-12-08 20:16:29 +0200

    create pr

--- .github/scripts/generate_manifests.sh
+++ .github/scripts/generate_manifests.sh
@@ -12,6 +12,6 @@ mkdir -p ${GITHUB_WORKSPACE}/manifests/akeyless-secrets-injection
 cp ${GITHUB_WORKSPACE}/manifest_work_dir/akeyless-secrets-injection/manifests/* ${GITHUB_WORKSPACE}/manifests/akeyless-secrets-injection/
 cp ${GITHUB_WORKSPACE}/manifest_work_dir/akeyless-secrets-injection/README.md ${GITHUB_WORKSPACE}/manifests/akeyless-secrets-injection/
 
-branch_name="generate-manifest--${GITHUB_SHA:0:8}"
+branch_name="generate-manifest-${GITHUB_SHA:0:8}"
 echo "branch_name=$branch_name" >> $GITHUB_ENV
 
