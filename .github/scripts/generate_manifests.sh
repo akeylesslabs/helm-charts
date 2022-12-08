12eafa35f3c6f9f96e85f69d84d7a50292241016
omriezra <96770150+omriezra@users.noreply.github.com>
2022-12-08 17:50:11 +0200

    add script to test update (#138)
    * add script to test update
    
    * update generated manifests
    
    * add script to test update
    
    Co-authored-by: omriezra <action@github.com>

---
+++ .github/scripts/generate_manifests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+dir_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "${dir_path}/common.sh"
+
+
+echo "${GITHUB_WORKSPACE}"
+
+mkdir -p ${GITHUB_WORKSPACE}/manifests/akeyless-secrets-injection
+cp ${GITHUB_WORKSPACE}/manifest_work_dir/akeyless-secrets-injection/manifests/* ${GITHUB_WORKSPACE}/manifests/akeyless-secrets-injection/
+cp ${GITHUB_WORKSPACE}/manifest_work_dir/akeyless-secrets-injection/README.md ${GITHUB_WORKSPACE}/manifests/akeyless-secrets-injection/
+
+message="update generated manifests"
+path="${GITHUB_WORKSPACE}/manifests/akeyless-secrets-injection/*"
+
+commit_and_push_to_git "${message}" "${path}"
+
